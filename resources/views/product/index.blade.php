1e91a8964fb633a90899f1f5b3e95ea5c1e53035
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-04-11 11:54:35 +0700

    Perubahan Ui

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -44,19 +44,32 @@
                                 @forelse($products as $product)
                                     <tr class="border-b border-gray-200 dark:border-gray-700">
                                         <td class="p-4">{{ $product->name }}</td>
-                                        <td class="p-4">{{ $product->quantity }}</td>
+                                        <td class="p-4">{{ $product->qty }}</td>
                                         <td class="p-4">Rp {{ number_format($product->price, 2, ',', '.') }}</td>
                                         <td class="p-4">{{ $product->user->name ?? $product->user_id }}</td>
                                         <td class="p-4 flex gap-3">
-                                            <a href="{{ route('product.show', $product->id) }}" class="text-blue-500 hover:underline">View</a>
+                                            <a href="{{ route('product.show', $product->id) }}" class="p-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md flex items-center justify-center transition-colors" title="View">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                                </svg>
+                                            </a>
                                             @can('update', $product)
-                                            <a href="{{ route('product.edit', $product->id) }}" class="text-yellow-500 hover:underline">Edit</a>
+                                            <a href="{{ route('product.edit', $product->id) }}" class="p-2 bg-yellow-500 hover:bg-yellow-600 text-white rounded-md flex items-center justify-center transition-colors" title="Edit">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                                </svg>
+                                            </a>
                                             @endcan
                                             @can('delete', $product)
                                             <form action="{{ route('product.delete', $product->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this product?');">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="text-red-500 hover:underline">Delete</button>
+                                                <button type="submit" class="p-2 bg-red-500 hover:bg-red-600 text-white rounded-md flex items-center justify-center transition-colors" title="Delete">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </button>
                                             </form>
                                             @endcan
                                         </td>
